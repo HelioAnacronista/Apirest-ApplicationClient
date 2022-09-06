1dbc4f5ac53b600122386ca83df64d29419bb3fb
Helio <heliojobs25@gmail.com>
2022-09-06 16:13:36 -0300

    7. Implementação do UserDetailsService (login pelo UserDetalis)

--- src/main/java/io/github/helio/config/SecurityConfig.java
+++ src/main/java/io/github/helio/config/SecurityConfig.java
@@ -1,7 +1,8 @@
 package io.github.helio.config;
 
+import io.github.helio.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UsuarioServiceImpl userService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -19,11 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("fulano")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER", "ADMIN");
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
     }
 
 
@@ -32,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("api/clientes/**")
+                .antMatchers("/api/clientes/**")
                 .hasAnyAuthority("USER", "ADMIN")
                 .antMatchers("/api/pedidos/**")
                 .hasAnyAuthority("USER", "ADMIN")
